5fe559d673bf92ac74724e63c75f6f78349cabc9
Carlos Eduardo <ocarlls.eduardo@gmail.com>
2023-04-24 15:21:26 -0300

    Configurada a conexão com o MongoDB Atlas e criado o model com as informações de um usuário.

--- server/index.js
+++ server/index.js
@@ -1,13 +1,28 @@
 const express = require('express');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(express.json())
-
-mongoose.connect("mongodb+srv://ocarlls:03122020@crud.rzbzlye.mongodb.net/?retryWrites=true&w=majority", {
+const UsuarioModel = require("./models/Usuario");
+//Conectando ao banco de dados Mongo
+mongoose.connect("mongodb+srv://ocarlls:03122020@crud.rzbzlye.mongodb.net/?retryWrites=true&w=majority", 
+{
     useNewUrlParser: true
 })
 
+app.get('/', async (req, res)=>{
+    const usuario = new UsuarioModel({
+        nome: 'Carlos',
+        login: 'ocarlls',
+        email: 'carlin@gmail.com'
+    });
+    try{
+        await usuario.save();
+        res.send('Usuario salvo com sucesso...');
+    } catch(err){
+        console.log('Erro ao salvar ' + err)
+    }
+})
 
 app.listen(3001, ()=>{
     console.log('Server rodando na porta 3001...');
---
+++ server/models/Usuario.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+// Definindo o model das informações de um usuario
+// a "tabela" que será criada
+const UsuarioSchema = new mongoose.Schema({
+    nome: {
+        type: String,
+        require: false
+    },
+    login: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+});
+
+//Definindo a collection "Database" que irá armazenar usuario
+const Usuario = mongoose.model("UserInfos", UsuarioSchema)
+module.exports = Usuario
